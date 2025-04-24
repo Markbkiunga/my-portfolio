9aa151ab67d9670e45fdf1ef1368af63cacdb704
Mark Brian <markbkiunga2017@gmail.com>
2025-04-24 13:56:38 +0300

    `Update LoadingScreen.js to use local image asset`

--- src/LoadingScreen.js
+++ src/LoadingScreen.js
@@ -3,10 +3,7 @@ import './LoadingScreen.css';
 const LoadingScreen = () => {
   return (
     <div id="loading-screen-container">
-      <img
-        alt="mark-logo"
-        src="https://markbriankiungakamau.co.ke/static/media/mark-logo.0f33804fda1536c25080.png"
-      />
+      <img alt="mark-logo" src="/mark-logo.avif" />
     </div>
   );
 };
